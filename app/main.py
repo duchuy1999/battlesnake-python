d59b82f9324ecb3c577ea26a707ecadb7b0c5264
Yichun Zhao <zyc5026@hotmail.com>
2018-03-03 13:05:52 -0800

    added to example code

--- app/main.py
+++ app/main.py
@@ -3,6 +3,41 @@ import os
 import random
 
 
+#codes adapted from https://github.com/terahn/battlesnake-bigasp
+def move(data, move):
+    myCoor = data['you']['body']['data']
+    myLen = data['you']['length']
+
+    if (move == 'up'):
+        nextMove = [myCoor[0]['x'], myCoor[0]['y'] - 1]
+    elif (move == 'down'):
+        nextMove = [myCoor[0]['x'], myCoor[0]['y'] + 1]
+    elif (move == 'left'):
+        nextMove = [myCoor[0]['x'] - 1, myCoor[0]['y']]
+    else:
+        nextMove = [myCoor[0]['x'] + 1, myCoor[0]['y']]
+
+	#snake won't run into itself
+    for i in range(1, myLen):
+        if (nextMove[0] == myCoor[i]['x'] and nextMove[1] == myCoor[i]['y']):
+            return False
+        
+    #snake won't run into walls
+    if ((nextMove[0] == data['width']) or (nextMove[0] == -1) or (nextMove[1] == data['height']) or (nextMove[1] == -1)):
+        return False
+
+    #snake won't run into other snakes
+    for i in range(len(data['snakes']['data'])):
+        for j in range(len(data['snakes']['data'][i]['body']['data'])):
+            enemyX = data['snakes']['data'][i]['body']['data'][j]['x']
+            enemyY = data['snakes']['data'][i]['body']['data'][j]['y']
+            if (nextMove[0] == enemyX and nextMove[1] == enemyY):
+                return False
+
+    return True
+
+
+
 @bottle.route('/static/<path:path>')
 def static(path):
     return bottle.static_file(path, root='static/')
@@ -34,12 +69,45 @@ def start():
 def move():
     data = bottle.request.json
 
-    # TODO: Do things with data
     directions = ['up', 'down', 'left', 'right']
 
+	global goX, goY, myX, myY
+    
+    #x and y coordinates of snake's head
+    myX = data['you']['body']['data'][0]['x']
+    myY = data['you']['body']['data'][0]['y']
+
+    #set go to location to closest food
+	closestDist = 1000
+
+    for i in range(len(data['food']['data'])):
+        foodX = data['food']['data'][i]['x']
+        foodY = data['food']['data'][i]['y']
+        distX = abs(myX - foodX)
+        distY = abs(myY - foodY)
+        dist = distX + distY
+
+        if (dist < closestDist):
+            closestDist = dist
+            goX = foodX
+            goY = foodY
+	
+	moveX = myX - goX
+	moveY = myY - goY
+
+	#call def move to check if it is safe to move
+    if (moveY > 0 and move(data, 'up')):
+        return 'up'
+    elif (moveY < 0 and move(data, 'down')):
+        return 'down'
+    elif (moveX > 0 and move(data, 'left')):
+        return 'left'
+    elif (moveX < 0 and move(data, 'right')):
+        return 'right'
+	
     return {
         'move': random.choice(directions),
-        'taunt': 'battlesnake-python!'
+        'taunt': 'Yichun and Huy have won the game!'
     }
 
 
